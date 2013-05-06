b99d6079e655f6fb54754c18f6102533e7c14306
Joel Richards <joelr@everydayhero.com.au>
2013-05-06 16:37:25 +1000

    remove deprecated methods

--- lib/koala/utils.rb
+++ lib/koala/utils.rb
@@ -16,18 +16,5 @@ module Koala
     self.logger = Logger.new(STDOUT)
     self.logger.level = Logger::ERROR
 
-    # @private
-    DEPRECATION_PREFIX = "KOALA: Deprecation warning: "
-
-    # Prints a deprecation message.
-    # Each individual message will only be printed once to avoid spamming.
-    def deprecate(message)
-      @posted_deprecations ||= []
-      unless @posted_deprecations.include?(message)
-        # only include each message once
-        Kernel.warn("#{DEPRECATION_PREFIX}#{message}")
-        @posted_deprecations << message
-      end
-    end
   end
 end
