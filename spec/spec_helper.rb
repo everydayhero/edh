80056c60baf221d9d1de81d751c9eda9de258962
Joel Richards <joelr@everydayhero.com.au>
2013-05-06 16:13:22 +1000

    remove file assets

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,5 +26,3 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 # set up our testing environment
 # load testing data and (if needed) create test users or validate real users
 KoalaTest.setup_test_environment!
-
-BEACH_BALL_PATH = File.join(File.dirname(__FILE__), "fixtures", "beach.jpg")
